3d6ecae36250fca6c7e990029a5990bbd4b32ac6
coderkkSk <147696411+coderkkSk@users.noreply.github.com>
2023-12-08 14:25:52 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = int(1604400)
-API_HASH = "8e6598bb138e8e7890b0fa19d6a152f6"
-BOT_TOKEN = "6651017840:AAFmdHWRjthzG2lfevZnAiPIPYORGFllOmQ"
-SESSION = "BQAYezAANQwPVO7Jqd_J3SEk52YjVBORnBFwSY3QOiEiPlY7YLpIkCDE6LNZJz1ge5z0MLbYCrVrb2b15YqGtca_z9AC-OqjpiTj_dmiNwFo_e9peXTHME06zewEToIz_dZ6riZFFAuNn-GtoM-rapwOVxtD96_JhoaluNHrU5qVwQLNTrUp-nxOiaoJs2BRB00cV_LCpLAx1pDV9aahc0H4hAYLmuEy5NAGVlUW6f03VSN9GkyKp1KX2f7geN67Guv6YMbRM6Sg8huG0PpVPZlitO4GOLyUKK_QAF0Z9qPUFue7rGAw3e-58mpwecjkwYFVatqAyDi4fmlxfnr2T0kTb0WghQAAAAGGJj8xAA"
-FORCESUB = "jogumotaru"
-AUTH = int(6545620785)
+API_ID = config("API_ID", default=None, cast=int)
+API_HASH = config("API_HASH", default=None)
+BOT_TOKEN = config("BOT_TOKEN", default=None)
+SESSION = config("SESSION", default=None)
+FORCESUB = config("FORCESUB", default=None)
+AUTH = config("AUTH", default=None, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
