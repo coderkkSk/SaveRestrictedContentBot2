11d5e03b28b879ef873a71bf2d41542d14ccd39f
coderkkSk <147696411+coderkkSk@users.noreply.github.com>
2023-12-12 20:10:41 +0530

    Update pyroplug.py

--- main/plugins/pyroplug.py
+++ main/plugins/pyroplug.py
@@ -38,12 +38,12 @@ async def get_msg(userbot, client, bot, sender, edit_id, msg_link, i):
             chat = str(msg_link.split("/")[-2])
         else:
             chat = str(msg_link.split("/")[-2])
-            if len(chat)  == 11 or len(chat) > 11:
-                chat = await client.get_chat(msg_link.split("/")[-2])
-                chat = chat.id
-            elif len(chat) > 3 :
+            if len(chat) > 3 and len(chat):
                 #chat =  int('-100' + str(client.get_chats(msg_link.split("/")[-2])))
                  chat = int('-100' + str(msg_link.split("/")[-2]))
+            elif len(chat)  == 11 or len(chat) > 11:
+                chat = await client.get_chat(msg_link.split("/")[-2])
+                chat = chat.id
             else:
                 chat =int('-100' + str(msg_link.split("/")[-3]))
         file = ""
